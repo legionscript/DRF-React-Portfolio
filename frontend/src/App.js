26a7cca3a1c4e8ffafbe35d1e1cb6426ed6fe71d
legionscript <legionscript@legionscript.com>
2022-06-20 10:50:11 -0400

    Tutorial 1

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='w-1/2 mx-auto text-center text-5xl'>
+      Home
+    </div>
+  );
+}
+
+export default App;
